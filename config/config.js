79cf10e8da547c24c674f703c2de56e8b11e3cd1
Claudia <claudx6@gmail.com>
2024-04-29 19:06:51 +0200

    URI agregada en config.js

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+// agregamos la cadena de conexión a MongoDB
 module.exports = {
-  mongoURI: ""
+  mongoURI: "mongodb+srv://claudia:dyNOn1QM7vSsQW46@cluster0.uk03zqc.mongodb.net/products?retryWrites=true&w=majority"
 };
