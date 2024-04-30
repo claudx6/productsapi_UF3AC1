30275858b5d27fb7caa4541d09a20c370f297f46
Claudia <claudx6@gmail.com>
2024-04-30 13:42:55 +0200

    cambios en package añadido build:css, build

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const products_routes = require('./routes/products.js');
-const slugify = require('slugify');
+// const slugify = require('slugify');
 
 // Server instantiation
 const app = express();
@@ -14,13 +14,13 @@ app.use(express.static('/views'));
 app.use(express.json());
 
 // mensaje de bienvenida
-const mensaje = "Welcome to Products API! Register to get API key! (to provide it, place it on header 'api-key' or as URL param 'apikey') Operation API endpoint Register POST /api/users/register List all products GET /api/products List products by ID GET /api/products/:productID Create product POST /api/products Update product PUT /api/products/:productID Delete product DELETE /api/products/:productID";
-const mensajeSlugify = slugify(mensaje, { replacement: '*'});
+// const mensaje = "Welcome to Products API! Register to get API key! (to provide it, place it on header 'api-key' or as URL param 'apikey') Operation API endpoint Register POST /api/users/register List all products GET /api/products List products by ID GET /api/products/:productID Create product POST /api/products Update product PUT /api/products/:productID Delete product DELETE /api/products/:productID";
+// const mensajeSlugify = slugify(mensaje, { replacement: '*'});
 
 // Routes
-app.get('/api', (req, res) => {
-  res.send(mensajeSlugify);
-});
+// app.get('/api', (req, res) => {
+//   res.send(mensajeSlugify);
+// });
 
 app.use('/', products_routes);
 
