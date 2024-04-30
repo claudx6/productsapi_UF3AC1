d30fc95577e9533a6705fe6c57d6ac6626ba8c84
Claudia <claudx6@gmail.com>
2024-04-30 14:02:25 +0200

    cambios en la URI

--- config/config.js
+++ config/config.js
@@ -1,4 +1,6 @@
 // agregamos la cadena de conexión a MongoDB
-module.exports = {
-  mongoURI: "mongodb+srv://claudia:dyNOn1QM7vSsQW46@cluster0.uk03zqc.mongodb.net/products?retryWrites=true&w=majority"
+const config = {
+  mongoURI: process.env.MONGOURI
 };
+
+module.exports = config;
